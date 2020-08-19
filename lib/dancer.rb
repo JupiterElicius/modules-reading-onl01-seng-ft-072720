f94e9f26ecf6765c57a90513b8d55d4b28669900
Nick Bazaldua <bazalnick@gmail.com>
2020-08-19 16:57:57 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,7 +4,7 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::ClassMethods
-  
+  include Dance
   attr_accessor :name
  
   def initialize(name)
