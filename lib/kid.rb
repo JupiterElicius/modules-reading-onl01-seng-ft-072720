85ad3770b43cef0b199907c02cc0954543977c9e
Nick Bazaldua <bazalnick@gmail.com>
2020-08-19 16:58:05 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,7 @@ require_relative './fancy_dance.rb'
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::ClassMethods
+  include Dance
   attr_accessor :name
   
   def initialize(name)
